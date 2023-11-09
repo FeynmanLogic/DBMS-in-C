a54f8dc7ad87a5813e6741d586caad91b5145006
FeynmanLogic <112418441+FeynmanLogic@users.noreply.github.com>
2023-11-09 17:20:28 +0530

    Added initial code

---
+++ 1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+void Showall(FILE* fileptr)
+{
+    char tempbuffer[100];
+    while (fgets(tempbuffer, sizeof(tempbuffer), fileptr)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", tempbuffer); 
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(tempbuffer);
+}
+int main() {
+   
+    FILE *fileptr = NULL;
+    fileptr = fopen("1.txt", "r");
+
+    if (fileptr == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    int operation;
+    char buffer[100];
+    printf("Let's implement a basic DBMS application in C\n which has CRUD Functionality\n");
+    printf("Enter 1 for Creating entry\n2 for Reading entry\n3 for Updating\n4 for Delete and \n5 for show database:\t");
+    scanf("%d",&operation);
+    switch(operation)
+    {
+        case 5:
+        Showall(fileptr);
+        break;
+    }
+    fclose(fileptr);
+
+    return 0;
+}
